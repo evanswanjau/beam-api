f29ddf6ae23f24e2af42396b72dec23cb3cdb0db
Evans Wanjau <evanswanjau@gmail.com>
2022-11-11 17:54:47 +0300

    Added route to handle file upload

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import fastify from "fastify";
+import { uploadViaUrl } from "./helpers/images";
 
 const server = fastify();
 
 server.get("/", async () => {
     return { hello: "world" };
+});
+
+server.post("/upload/", async (req: any, res) => {
+    try {
+        res.send(await uploadViaUrl(req.body.url));
+    } catch (error) {
+        res.send(error);
+    }
 });
 
 server.listen({ port: 8080 }, (err, address) => {
